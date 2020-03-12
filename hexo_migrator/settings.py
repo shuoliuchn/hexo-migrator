2ddd382a74f8367911b87e8cbcf1372e4161e95d
刘硕 <1696146913@qq.com>
2020-03-12 19:54:08 +0800

    设置微调

--- hexo_migrator/settings.py
+++ hexo_migrator/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@(smtcxz*u+_6a=$lxusc=#mq40)l$j*kdj412+r%@@ko#6a$-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
